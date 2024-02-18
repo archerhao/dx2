f4e06f12bb35745bab4b008a9a89c6618024afc7
archerhao <28726263@qq.com>
2024-02-18 17:04:39 +0800

    first edition

--- dx2/dx2.cpp
+++ dx2/dx2.cpp
@@ -3,8 +3,34 @@
 
 #include "framework.h"
 #include "dx2.h"
+#include <wrl.h>
+#include <strsafe.h>
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+#include <d3d12.h>
+#include <d3d12shader.h>
+#include <d3dcompiler.h>
+#if defined(_DEBUG)
+#include <dxgidebug.h>
+#endif
+
+using namespace Microsoft;
+using namespace Microsoft::WRL;
+using namespace DirectX;
+
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "d3dcompiler.lib")
 
 #define MAX_LOADSTRING 100
+#define THROW_IF_FAILED(hr) {HRESULT _hr = (hr); if(FAILED(_hr)) { throw _hr;}}
+
+struct GRS_VERTEX
+{
+    XMFLOAT4 m_vtPos;
+    XMFLOAT4 m_vtColor;
+};
 
 // 全局变量:
 HINSTANCE hInst;                                // 当前实例
@@ -13,7 +39,7 @@ WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
 
 // 此代码模块中包含的函数的前向声明:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
+BOOL                InitInstance(HINSTANCE, int, HWND&);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
@@ -25,7 +51,46 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: 在此处放置代码。
+    const UINT nFrameBackBufCount = 3u;
+    int iWidth = 1024;
+    int iHeight = 768;
+    UINT nFrameIndex = 0;
+
+    DXGI_FORMAT emRenderTargetFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+    const float faClearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+    UINT nDXGIFactoryFlags = 0u;
+    UINT nRTVDescriptorSize = 0u;
+
+    HWND hWnd = nullptr;
+    TCHAR pszAppPath[MAX_PATH] = {};
+
+    float fTriangleSize = 3.0f;
+
+    D3D12_VERTEX_BUFFER_VIEW stVertexBufferView = {};
+
+    UINT64 n64FenceValue = 0ui64;
+    HANDLE hEventFence = nullptr;
+
+    D3D12_VIEWPORT stViewPort = { 0.0f, 0.0f, static_cast<float>(iWidth), static_cast<float>(iHeight) };
+    D3D12_RECT stScissorRect = { 0, 0, static_cast<LONG>(iWidth), static_cast<LONG>(iHeight) };
+
+    D3D_FEATURE_LEVEL emFeatureLevel = D3D_FEATURE_LEVEL_12_1;
+
+    ComPtr<IDXGIFactory5> pIDXGIFactory5;
+    ComPtr<IDXGIAdapter1> pIAdapter1;
+    ComPtr<ID3D12Device4> pID3D12Device4;
+    ComPtr<ID3D12CommandQueue> pICMDQueue;
+    ComPtr<IDXGISwapChain1> pISwapChain1;
+    ComPtr<IDXGISwapChain3> pISwapChain3;
+    ComPtr<ID3D12DescriptorHeap> pIRTVHeap;
+    ComPtr<ID3D12Resource> pIARenderTargets[nFrameBackBufCount];
+    ComPtr<ID3D12RootSignature> pIRootSignature;
+    ComPtr<ID3D12PipelineState> pIPipelineState;
+
+    ComPtr<ID3D12CommandAllocator> pICMDAlloc;
+    ComPtr<ID3D12GraphicsCommandList> pICMDList;
+    ComPtr<ID3D12Resource> pIVertexBuffer;
+    ComPtr<ID3D12Fence> pIFence;
 
     // 初始化全局字符串
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -33,10 +98,46 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     MyRegisterClass(hInstance);
 
     // 执行应用程序初始化:
-    if (!InitInstance (hInstance, nCmdShow))
+    if (!InitInstance (hInstance, nCmdShow, hWnd))
     {
         return FALSE;
     }
+
+    //得到当前的工作目录，方便我们使用相对路径来访问各种资源文件
+    if (::GetModuleFileName(nullptr, pszAppPath, MAX_PATH) == 0)
+    {
+        THROW_IF_FAILED(HRESULT_FROM_WIN32(GetLastError()));
+    }
+    WCHAR* lastSlash = _tcsrchr(pszAppPath, _T('\\'));
+    //删除exe文件名
+    if (lastSlash)
+    {
+        *lastSlash = 0;
+    }
+    //删除x64路径
+    lastSlash = _tcsrchr(pszAppPath, _T('\\'));
+    if (lastSlash)
+    {
+        *lastSlash = 0;
+    }
+    //删除debug或release路径
+    lastSlash = _tcsrchr(pszAppPath, _T('\\'));
+    if (lastSlash)
+    {
+        *lastSlash = 0;
+    }
+
+    //打开显示子系统的调试支持
+#if defined(_DEBUG)
+    ComPtr<ID3D12Debug> debugController;
+    if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+    {
+        debugController->EnableDebugLayer();
+        nDXGIFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+    }
+#endif
+
+    //创建DXGI Factory对象
 
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DX2));
 
@@ -75,7 +176,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DX2));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.hbrBackground  = (HBRUSH)(GetStockObject(NULL_BRUSH));
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DX2);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -93,11 +194,11 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //        在此函数中，我们在全局变量中保存实例句柄并
 //        创建和显示主程序窗口。
 //
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, HWND& hWnd)
 {
    hInst = hInstance; // 将实例句柄存储在全局变量中
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+   hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPED,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
